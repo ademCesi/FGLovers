e346569721600c029593139b39145b8b3a674622
Mathis Noyer <mathis.noyer49@gmail.com>
2025-12-05 03:38:59 +0100

    feat(main-page): add main content

--- fglovers/src/assets/index.ts
+++ fglovers/src/assets/index.ts
@@ -1,5 +1,15 @@
 import climbingShoe from "./icons/climbing-shoe-transparent.png";
+import carabiner from "./icons/carabiner-transparent.png";
+import hold from "./icons/hold-transparent.png";
+
+import topographicBackground from "./images/2d-topographic-background.jpg";
 
 export const Icons = {
     climbingShoe: climbingShoe,
+    carabiner: carabiner,
+    hold: hold,
+};
+
+export const Images = {
+    topographicBackground: topographicBackground,
 };
---
+++ fglovers/src/components/Content4/Content4.tsx
@@ -0,0 +1,18 @@
+import './Content4.css';
+
+export default function Content4() {
+    return (
+        <div className="content-container4">
+            <h2>Votre Mission : Construire votre Village Résistant</h2>
+            <h3>Vous êtes le directeur d’un établissement scolaire. Votre objectif : le faire entrer dans la démarche NIRD, étape par étape, en résistant aux pièges des Big Tech.</h3>
+            <p>L’utilisateur devra faire des choix :</p>
+            <ul>
+                <li>Installer Linux ou payer Windows</li>
+                <li>Réparer un PC ou en acheter un neuf</li>
+                <li>Utiliser LibreOffice ou une suite propriétaire</li>
+                <li>Héberger les données localement ou chez un hyperscaler</li>
+                <li>Mutualiser des ressources ou acheter un logiciel fermé</li>
+            </ul>
+        </div>
+    );
+}
